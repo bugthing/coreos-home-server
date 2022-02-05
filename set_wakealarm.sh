8143069198d7dc6dbbeaf7ad8c8bef8bcf25405b
Benjamin Martin <ben.martin@smartpension.co.uk>
2022-02-05 20:51:29 +0000

    Change set_wakealarm.sh to use rtcwake command

--- set_wakealarm.sh
+++ set_wakealarm.sh
@@ -3,25 +3,23 @@
 # This script sets the linux wakealarm so that if the machine is
 # shutdown, it will turn back on at the specified time.
 
-function wake_up_time(){
-    local tomorrows_date="$(date --date='tomorrow' +%Y-%m-%d)"
-    local wake_up_time=$(date -d "$tomorrows_date $1" +%s)
-    echo "$wake_up_time"
+function tomorrow(){
+    echo "$(date --date='tomorrow' +%Y-%m-%d)"
 }
 
-alarm_for=$(wake_up_time "07:00")
+alarm_for="$(tomorrow) 07:00"
 
-if [[ $(date -d @$alarm_for +%u) -gt 5 ]]; then
+if [[ $(date -d "$(tomorrow)" +%u) -gt 5 ]]; then
   # awaking on the wkend..
-  alarm_for=$(wake_up_time "07:30")
+  alarm_for="$(tomorrow) 07:30"
 fi
 
-echo Waking up at $(date -d @$alarm_for +'%Y-%m-%d %T')
+echo Setting wake alarm for: $alarm_for
 
-echo Setting wake alarm with: $alarm_for
-
-echo $alarm_for > /sys/class/rtc/rtc0/wakealarm
+rtcwake -m no --date="$alarm_for" --utc --dry-run
 
 echo Alarm details:
 
 cat /proc/driver/rtc
+
+rtcwake -m show
